f01b4df75e70cd2d46e31ffa08ed393ce8b34504
MacRat <m@crat.jp>
2021-01-30 17:57:13 +0900

    feat: add status to ldapin_http_latency metrics

--- metrics/http.go
+++ metrics/http.go
@@ -14,7 +14,7 @@ var (
 			Name:      "latency",
 			Help:      "The whole latency of each endpoint. It is includes non-core process like minifying or compression.",
 		},
-		[]string{"method", "path"},
+		[]string{"method", "path", "status"},
 	)
 )
 
@@ -22,13 +22,52 @@ func init() {
 	prometheus.MustRegister(HTTPLatency)
 }
 
+type ResponseCollcetor struct {
+	Upstream http.ResponseWriter
+	Code     int
+}
+
+func (w *ResponseCollcetor) Header() http.Header {
+	return w.Upstream.Header()
+}
+
+func (w *ResponseCollcetor) Write(data []byte) (int, error) {
+	return w.Upstream.Write(data)
+}
+
+func (w *ResponseCollcetor) WriteHeader(statusCode int) {
+	w.Code = statusCode
+	w.Upstream.WriteHeader(statusCode)
+}
+
+func (w *ResponseCollcetor) StatusClass() string {
+	switch {
+	case w.Code >= 500:
+		return "5xx"
+	case w.Code >= 400:
+		return "4xx"
+	case w.Code >= 300:
+		return "3xx"
+	case w.Code >= 200:
+		return "2xx"
+	case w.Code >= 100:
+		return "1xx"
+	default:
+		return "unknown"
+	}
+}
+
 func Middleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rc := &ResponseCollcetor{
+			Upstream: w,
+		}
+
 		timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
-			HTTPLatency.WithLabelValues(r.Method, r.URL.Path).Observe(v)
+			HTTPLatency.WithLabelValues(r.Method, r.URL.Path, rc.StatusClass()).Observe(v)
 		}))
 		defer timer.ObserveDuration()
 
-		handler.ServeHTTP(w, r)
+		handler.ServeHTTP(rc, r)
 	})
 }
