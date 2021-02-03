7146a23269762cd7c1d3f71d2fba0aaf04a57cc6
MacRat <m@crat.jp>
2021-02-03 20:56:14 +0900

    test: add some small tests

--- config/duration_test.go
+++ config/duration_test.go
@@ -1,6 +1,7 @@
 package config_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/macrat/lauth/config"
@@ -9,19 +10,34 @@ import (
 func TestDuration(t *testing.T) {
 	d := config.Duration(0)
 
-	tests := []string{
-		"1w2d3h",
-		"1w3h",
-		"5m2s",
-		"8h",
-		"0",
+	tests := []struct {
+		Input   string
+		Output  string
+		Seconds int64
+	}{
+		{"1w2d3h", "1w2d3h", ((7+2)*24 + 3) * 60 * 60},
+		{"1w3h", "1w3h", (7*24 + 3) * 60 * 60},
+		{"5m2s", "5m2s", 5*60 + 2},
+		{"8h", "8h", 8 * 60 * 60},
+		{"2h0m", "2h", 2 * 60 * 60},
+		{"70m", "1h10m", 70 * 60},
+		{"0d0m", "0", 0},
+		{"0", "0", 0},
 	}
 
 	for _, tt := range tests {
-		if err := (&d).Set(tt); err != nil {
-			t.Errorf("failed to parse %s: %s", tt, err)
-		} else if s := d.String(); s != tt {
-			t.Errorf("unexpected unmarshal: expected %s but got %s", tt, s)
+		if err := (&d).Set(tt.Input); err != nil {
+			t.Errorf("failed to parse %s: %s", tt.Input, err)
+			continue
+		}
+		if s := d.String(); s != tt.Output {
+			t.Errorf("%s: unexpected unmarshal: expected %s but got %s", tt.Input, tt.Output, s)
+		}
+		if i := d.IntSeconds(); i != tt.Seconds {
+			t.Errorf("%s: unexpected int seconds: expected %d but got %d", tt.Input, tt.Seconds, i)
+		}
+		if s := d.StrSeconds(); s != fmt.Sprint(tt.Seconds) {
+			t.Errorf("%s: unexpected int seconds: expected %#v but got %#v", tt.Input, tt.Seconds, s)
 		}
 	}
 }
---
+++ page/templates_test.go
@@ -0,0 +1,76 @@
+package page_test
+
+import (
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/macrat/lauth/config"
+	"github.com/macrat/lauth/page"
+)
+
+func Render(t *testing.T, tmpl *template.Template, name string) string {
+	t.Helper()
+
+	buf := bytes.NewBuffer([]byte{})
+
+	if err := tmpl.ExecuteTemplate(buf, name, map[string]map[string]string{"error": {"Reason": ""}}); err != nil {
+		t.Fatalf("failed to render: %s", err)
+	}
+
+	if len(buf.Bytes()) == 0 {
+		t.Fatalf("failed to render: rendered HTML size was 0")
+	}
+
+	return string(buf.Bytes())
+}
+
+func MakeTestFile(t *testing.T, content string) string {
+	t.Helper()
+
+	f, err := ioutil.TempFile("", "*")
+	if err != nil {
+		t.Fatalf("failed to prepare test file: %s", err)
+	}
+	_, err = f.Write([]byte(content))
+	if err != nil {
+		t.Fatalf("failed to write test file: %s", err)
+	}
+
+	return f.Name()
+}
+
+func TestLoad(t *testing.T) {
+	tmpl, err := page.Load(config.TemplateConfig{LoginPage: "", ErrorPage: ""})
+	if err != nil {
+		t.Fatalf("failed to load templates: %s", err)
+	}
+
+	if Render(t, tmpl, "login.tmpl") == "[[this is test login page]]" {
+		t.Errorf("expected normal builtin login page but got test page")
+	}
+
+	if Render(t, tmpl, "error.tmpl") == "[[this is test error page]]" {
+		t.Errorf("expected normal builtin error page but got test page")
+	}
+
+	loginPage := MakeTestFile(t, "[[this is test login page]]")
+	defer os.Remove(loginPage)
+	errorPage := MakeTestFile(t, "[[this is test error page]]")
+	defer os.Remove(errorPage)
+
+	tmpl, err = page.Load(config.TemplateConfig{LoginPage: loginPage, ErrorPage: errorPage})
+	if err != nil {
+		t.Fatalf("failed to load templates: %s", err)
+	}
+
+	if Render(t, tmpl, "login.tmpl") != "[[this is test login page]]" {
+		t.Errorf("expected test login page but got normal builtin page")
+	}
+
+	if Render(t, tmpl, "error.tmpl") != "[[this is test error page]]" {
+		t.Errorf("expected test error page but got normal builtin page")
+	}
+}
